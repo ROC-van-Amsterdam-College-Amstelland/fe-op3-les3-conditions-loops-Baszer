6103e31a9cac754460bf9f0739dfcaea9cf02551
baszer <basfvega26@gmail.com>
2019-11-28 16:15:21 +0100

    Opdracht 1 en 2

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+function getSaldo()
+{
+    var mySaldo = prompt("HOEVEEL GELD HEB JE")
+    if(mySaldo >= 25){
+        alert("je hebt genoeg geld")
+    
+    
+    }
+
+    else{
+        alert("Onvoldoende saldo")
+    }
+}
