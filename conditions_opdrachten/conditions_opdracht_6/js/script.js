897048935a9cdd530ad5f31701fc9b0ad23ff698
baszer <basfvega26@gmail.com>
2019-12-09 13:54:42 +0100

    opdracht 6 conditions

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,14 @@
-//javascript code
+function insert(num){
+  document.form.textview.value = document.form.textview.value+num
+}
+function equal(){
+  var exp = document.form.textview.value
+  if(exp){
+      document.form.textview.value = eval(exp)
+  }
+}
+
+function back(){
+  var exp = document.form.textview.value
+  document.form.textview.value = exp.substring(0,exp.length-1)
+}
